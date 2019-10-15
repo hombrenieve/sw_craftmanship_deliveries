d1ddfb26973867c543af975ef93dbea721f68bda
Pablo Rebaque Diaz <hombrenieve@gmail.com>
2019-10-15 23:53:27 +0200

    New delivery sends the form with all items
    We still have to read the form

--- src/main/resources/static/js/newDelivery.js
+++ src/main/resources/static/js/newDelivery.js
@@ -1,7 +1,19 @@
-function addElement() {
-    var text = document.getElementById('element');
+function addItem() {
+    var text = document.getElementById('item');
     if(text.value) {
-        document.getElementById('elementList').innerHTML += '<li>'+text.value+'</li>';
+        document.getElementById('itemList').innerHTML += '<li class="list-item">'+text.value+'</li>';
         text.value = '';
+    }
+}
+
+function completeForm() {
+    var form = document.getElementById('newDeliveryForm');
+    var list = document.getElementsByClassName('list-item');
+    for(var i = 0; i < list.length; i++) {
+        var input = document.createElement('input');
+        input.setAttribute('name', 'item'+i);
+        input.setAttribute('value', list[i].innerHTML);
+        input.setAttribute('type', 'hidden');
+        form.appendChild(input);
     }
 }
