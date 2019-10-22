4150f7bdf2a9d41319164aa0457e76a963084508
Pablo Rebaque Diaz <hombrenieve@gmail.com>
2019-10-23 00:31:19 +0200

    Add template files

--- src/main/java/com/hombrenieve/deliveries/RootController.java
+++ src/main/java/com/hombrenieve/deliveries/RootController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RootController {
     @GetMapping
     public String redirection() {
-        return "list.html";
+        return "index.html";
     }
 }
