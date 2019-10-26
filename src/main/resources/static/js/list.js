6831a7c56adea50102f3e72e29b5fe8e0f60ef98
Pablo Rebaque Diaz <hombrenieve@gmail.com>
2019-10-27 00:16:14 +0200

    Modify the list js to use templates as well

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,11 +1,10 @@
 $(function () {
     $.getJSON('deliveries', function (data) {
         $.each(data, function (key, value) {
-            $("#shoppingLists").append('<li class="sidebar-list-item"><a href="#" class="sidebar-link text-muted" '
-                +'onclick="showShoppingList('+ value['id'] + ')"'
-                +'><i class="o-survey-1 mr-3 text-gray"></i><span>'
-                + value['name']
-                +'</span></a></li>');
+            var sideBarItem = getTemplate('#sidebarShoppingList');
+            sideBarItem.querySelector('a').setAttribute('onclick', 'showShoppingList('+value.id+')');
+            sideBarItem.querySelector('span').innerHTML = value.name;
+            $("#shoppingLists").append(sideBarItem);
         });
     });
 });
